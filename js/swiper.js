1c72f2181cb5403dd56f1050b6621bacc50c3c15
Y-youngchan <sunhama2000@naver.com>
2026-03-19 15:32:38 +0900

    20260319_bookstore

---
+++ js/swiper.js
@@ -0,0 +1,37 @@
+// # slider. swiper
+var swiper = new Swiper(".sliderSwiper", {
+    navigation: {
+        nextEl: "#slider .swiper-button-next",
+        prevEl: "#slider .swiper-button-prev",
+    },
+
+    autoplay: {
+        delay: 3000,
+        disableOnInteraction: false,
+    },
+
+    pagination: {
+        el: "#slider .swiper-pagination",
+        clickable: true,
+    },
+});
+
+
+
+// # new swiper
+var new_swiper = new Swiper(".newSwiper", {
+    slidesPerView: 5,
+    spaceBetween: 15,
+    pagination: {
+        el: "#new .swiper-pagination",
+        clickable: true,
+    },
+
+
+
+    navigation: {
+        nextEl: "#new .swiper-button-next",
+        prevEl: "#new .swiper-button-prev",
+    },
+    slidesPerGroup: 5
+});
